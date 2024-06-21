f1146de20b194cafc9ebf59f0094a5f3b2ebccfa
vucham203 <pimpompimpom4@gmail.com>
2024-06-21 14:04:00 +0700

    aa

---
+++ cod/connectToDatabase.php
@@ -0,0 +1,11 @@
+<?php
+// Database connection variables
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "DACS";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
